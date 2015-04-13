cfc958ee7d347436ba5ddd7f2fd26898cf51dce3
Nick Gammon <nick@gammon.com.au>
2015-04-14 06:40:41 +1000

    Initial commit

---
+++ I2C_Anything.h
@@ -0,0 +1,25 @@
+// Written by Nick Gammon
+// May 2012
+
+#include <Arduino.h>
+#include <Wire.h>
+
+template <typename T> unsigned int I2C_writeAnything (const T& value)
+  {
+    const byte * p = (const byte*) &value;
+    unsigned int i;
+    for (i = 0; i < sizeof value; i++)
+          Wire.write(*p++);
+    return i;
+  }  // end of I2C_writeAnything
+
+template <typename T> unsigned int I2C_readAnything(T& value)
+  {
+    byte * p = (byte*) &value;
+    unsigned int i;
+    for (i = 0; i < sizeof value; i++)
+          *p++ = Wire.read();
+    return i;
+  }  // end of I2C_readAnything
+
+
