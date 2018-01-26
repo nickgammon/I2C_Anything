9f06ad1765753e4a6f859be7290cce527d4d3273
Nick Gammon <nick@gammon.com.au>
2018-01-27 08:44:06 +1100

    Simplified write code

--- I2C_Anything.h
+++ I2C_Anything.h
@@ -6,11 +6,8 @@
 
 template <typename T> unsigned int I2C_writeAnything (const T& value)
   {
-    const byte * p = (const byte*) &value;
-    unsigned int i;
-    for (i = 0; i < sizeof value; i++)
-          Wire.write(*p++);
-    return i;
+  Wire.write((byte *) &value, sizeof (value));
+  return sizeof (value);
   }  // end of I2C_writeAnything
 
 template <typename T> unsigned int I2C_readAnything(T& value)
